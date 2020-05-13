d656609e0a5074fc1573d68159b96eafb877ab5b
Joao Reis <jreisstudio@gmail.com>
2020-05-13 16:03:07 -0300

    intial configuration

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div >
+    <div class="text-center">
+      <h1> Stop Flying Blind <span class="fa fa-paper-plane"></span> </h1>
+      <br/>
+      <h6 class=" text-secondary">A card game to understand and visualize Flight Levels System Architectures in action! </h6>
+    </div>
+  </div>
+
+</template>
+
+
+
+<script>
+import "bootstrap/dist/css/bootstrap.css"
+import "font-awesome/css/font-awesome.css"
+
+export default {
+  name: 'App'
+}
+</script>
+
+
+
+
+<style>
+
+
+</style>

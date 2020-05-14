4696fb8f90537a31674f5582490dfb0444e9368f
Joao Reis <jreisstudio@gmail.com>
2020-05-13 23:50:33 -0300

    starting cards implementation

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
-  <div >
+  <div id="app" >
+
     <div class="text-center">
       <h1> Stop Flying Blind <span class="fa fa-paper-plane"></span> </h1>
       <br/>
       <h6 class=" text-secondary">A card game to understand and visualize Flight Levels System Architectures in action! </h6>
     </div>
+
+    <Cards msg="Cartoes aqui" />
+
   </div>
 
 </template>
@@ -12,11 +16,16 @@
 
 
 <script>
+
+import Cards from "./components/Cards.vue"
+
 import "bootstrap/dist/css/bootstrap.css"
 import "font-awesome/css/font-awesome.css"
-
 export default {
-  name: 'App'
+  name: 'app',
+  components: {
+      Cards
+  }
 }
 </script>
 
---
+++ src/components/Cards.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="main" class="container">
+    <div class="row">
+        <div class="col-md">
+            <div class="card">
+                <div class="card-body">
+                  {{ msg }}
+                </div>
+
+            </div>
+        </div>
+
+        <div class="col-md">
+            <div class="card">
+                <div class="card-body">
+                  Opa...
+                </div>
+
+            </div>
+        </div>
+
+
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
